1df57250b6b981d896bb845c0d8bf36dd3790869
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2024-12-23 15:53:36 +0900

    agora追加

---
+++ frontend/ios/agora/agora/agoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  agoraApp.swift
+//  agora
+//
+//  Created by user on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct agoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
