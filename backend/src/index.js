11cb068657724d7347d723ac13bf03c9d5bd644e
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2024-12-24 09:42:21 +0900

    backend再作成
    Co-authored-by: yukoTake <yukotake@users.noreply.github.com>

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config({ path: "./.env" });
+
+const { setupServer } = require("./server");
+
+const app = setupServer();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on: http://localhost:${PORT}/`);
+});
