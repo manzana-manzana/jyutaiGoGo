7e0585a2f969a8f7d6f2383a4da6c3fe4ecf572c
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2025-01-07 09:38:01 +0900

    Location実装中

--- frontend/app/atom.ts
+++ frontend/app/atom.ts
@@ -1,17 +1,16 @@
-
 import { atom } from "jotai";
 
 export const uriAtom = atom<any>(null);
 
 //APIパス
-export const apiAddressAtom = atom<string>("http://192.168.1.13:2929")
+// export const apiAddressAtom = atom<string>("http://192.168.1.13:2929")
+export const apiAddressAtom = atom<string>(`http://192.168.11.5:8080`);
 
 //表示screen
-export const screenAtom = atom<string>("sec0_1")
+export const screenAtom = atom<string>("sec0_1");
 
 //渋滞に入ったかどうか（位置情報から設定）
-export const isJamAtom = atom<boolean>(false)
+export const isJamAtom = atom<boolean>(false);
 
 //会話するかどうか（ユーザーが選択）
-export const isTalkAtom = atom<boolean>(false)
-
+export const isTalkAtom = atom<boolean>(false);
