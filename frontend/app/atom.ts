d41656514b57a363faae3698e6ea9daa1b834646
yukotakeichi <yuko_murase_aa@mail.toyota.co.jp>
2025-01-08 20:58:24 +0900

    front4 動く車を複数台配置 動的にランダム色で配置

--- frontend/app/atom.ts
+++ frontend/app/atom.ts
@@ -14,16 +14,17 @@ type Groups = {
 // ???
 export const uriAtom = atom<any>(null);
 
-// APIパス
+//APIパス
+// export const apiAddressAtom = atom<string>("http://192.168.1.13:2929")
 export const apiAddressAtom = atom<string>(`http://192.168.1.2:2929`);
 
-// 表示screen
+//表示screen
 export const screenAtom = atom<string>("sec0_1");
 
-// 渋滞に入ったかどうか（位置情報から設定）
+//渋滞に入ったかどうか（位置情報から設定）
 export const isJamAtom = atom<boolean>(false);
 
-// 会話するかどうか（ユーザーが選択）
+//会話するかどうか（ユーザーが選択）
 export const isTalkAtom = atom<boolean>(false);
 
 // ルーム内の人数
