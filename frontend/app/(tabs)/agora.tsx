6d433d1d9334a64c276a5ea588c65ff5fa99e57e
yukotakeichi <yuko_murase_aa@mail.toyota.co.jp>
2025-01-09 16:30:40 +0900

    マージのために一旦こみっと

---
+++ frontend/app/(tabs)/agora.tsx
@@ -0,0 +1,186 @@
+
+
+// Import React Hooks
+import React, { useRef, useState, useEffect } from 'react';
+// Import user interface elements
+import {
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+// Import components related to obtaining Android device permissions
+import { PermissionsAndroid, Platform, Image } from 'react-native';
+// Import Agora SDK
+import {
+    createAgoraRtcEngine,
+    ChannelProfileType,
+    ClientRoleType,
+    IRtcEngine,
+    RtcConnection,
+    IRtcEngineEventHandler,
+} from 'react-native-agora';
+
+// Define basic information
+const appId = '12d6ce5a43d740298dc00619516bb4b2';
+const token = '007eJxTYEi5OYelrfhHSu5h5n6Zk6+X7O86LRZV+rVHSvJd24u9jE0KDIZGKWbJqaaJJsYp5iYGRpYWKckGBmaGlqaGZklJJklGpgsy0hsCGRmOPn7FzMgAgSA+K0NJcWl6KQMDAMPcIWM=';
+const channelName = 'tsugu';
+const uid = 0; // Local user Uid, no need to modify
+
+export default function HomeScreen_agora()  {
+    const agoraEngineRef = useRef<IRtcEngine>(); // IRtcEngine instance
+    const [isJoined, setIsJoined] = useState(false); // Whether the local user has joined the channel
+    const [isHost, setIsHost] = useState(true); // User role
+    const [remoteUid, setRemoteUid] = useState(0); // Uid of the remote user
+    const [message, setMessage] = useState(''); // User prompt message
+    const eventHandler = useRef<IRtcEngineEventHandler>(); // Callback functions
+    useEffect(() => {
+        // Initialize the engine when the App starts
+        setupVideoSDKEngine();
+        // Release memory when the App is closed
+        return () => {
+            agoraEngineRef.current?.unregisterEventHandler(eventHandler.current!);
+            agoraEngineRef.current?.release();
+        };
+    }, []);
+    // Define the setupVideoSDKEngine method called when the App starts
+    const setupVideoSDKEngine = async () => {
+        try {
+            // Create RtcEngine after obtaining device permissions
+            if (Platform.OS === 'android') {
+                await getPermission();
+            }
+            agoraEngineRef.current = createAgoraRtcEngine();
+            const agoraEngine = agoraEngineRef.current;
+            eventHandler.current = {
+                onJoinChannelSuccess: () => {
+                    showMessage('Successfully joined channel: ' + channelName);
+                    setIsJoined(true);
+                },
+                onUserJoined: (_connection: RtcConnection, uid: number) => {
+                    showMessage('Remote user ' + uid + ' joined');
+                    setRemoteUid(uid);
+                },
+                onUserOffline: (_connection: RtcConnection, uid: number) => {
+                    showMessage('Remote user ' + uid + ' left the channel');
+                    setRemoteUid(0);
+                },
+            };
+            // Register the event handler
+            agoraEngine.registerEventHandler(eventHandler.current);
+            // Initialize the engine
+            agoraEngine.initialize({
+                appId: appId,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    // Define the join method called after clicking the join channel button
+    const join = async () => {
+        if (isJoined) {
+            return;
+        }
+        try {
+            if (isHost) {
+                // Join the channel as a broadcaster
+                agoraEngineRef.current?.joinChannel(token, channelName, uid, {
+                    // Set channel profile to live broadcast
+                    channelProfile: ChannelProfileType.ChannelProfileCommunication,
+                    // Set user role to broadcaster
+                    clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+                    // Publish audio collected by the microphone
+                    publishMicrophoneTrack: true,
+                    // Automatically subscribe to all audio streams
+                    autoSubscribeAudio: true,
+                });
+            } else {
+                // Join the channel as an audience
+                agoraEngineRef.current?.joinChannel(token, channelName, uid, {
+                    // Set channel profile to live broadcast
+                    channelProfile: ChannelProfileType.ChannelProfileCommunication,
+                    // Set user role to audience
+                    clientRoleType: ClientRoleType.ClientRoleAudience,
+                    // Do not publish audio collected by the microphone
+                    publishMicrophoneTrack: false,
+                    // Automatically subscribe to all audio streams
+                    autoSubscribeAudio: true,
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    // Define the leave method called after clicking the leave channel button
+    const leave = () => {
+        try {
+            // Call leaveChannel method to leave the channel
+            agoraEngineRef.current?.leaveChannel();
+            setRemoteUid(0);
+            setIsJoined(false);
+            showMessage('Left the channel');
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    // Render user interface
+    return (
+        <SafeAreaView style={styles.main}>
+            <Text style={styles.head}>Agora Voice Calling Quickstart</Text>
+            <View style={styles.btnContainer}>
+                <Text onPress={join} style={styles.button}>
+                    Join Channel
+                </Text>
+                <Text onPress={leave} style={styles.button}>
+                    Leave Channel
+                </Text>
+            </View>
+            <ScrollView
+                style={styles.scroll}
+                contentContainerStyle={styles.scrollContainer}>
+                {isJoined ? (
+                    <Text>Local user uid: {uid}</Text>
+                ) : (
+                    <Text>Join a channel</Text>
+                )}
+                {isJoined && remoteUid !== 0 ? (
+                    <Text>Remote user uid: {remoteUid}</Text>
+                ) : (
+                    <Text>Waiting for remote user to join</Text>
+                )}
+                <Text>{message}</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+    // Display information
+    function showMessage(msg: string) {
+        setMessage(msg);
+    }
+};
+// Define user interface styles
+const styles = StyleSheet.create({
+    button: {
+        paddingHorizontal: 25,
+        paddingVertical: 4,
+        fontWeight: 'bold',
+        color: '#ffffff',
+        backgroundColor: '#0055cc',
+        margin: 5,
+    },
+    main: { flex: 1, alignItems: 'center' },
+    scroll: { flex: 1, backgroundColor: '#ddeeff', width: '100%' },
+    scrollContainer: { alignItems: 'center' },
+    videoView: { width: '90%', height: 200 },
+    btnContainer: { flexDirection: 'row', justifyContent: 'center' },
+    head: { fontSize: 20 },
+});
+const getPermission = async () => {
+    if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+    }
+};
+
+
---
+++ frontend/app/components/sec4_room_copy.tsx
@@ -0,0 +1,226 @@
+import React, {useEffect, useState} from 'react';
+import {Image, Pressable, StyleSheet, Text, View, Animated, Dimensions} from 'react-native';
+import {styles} from "@/app/style";
+// import {moderateScale} from "react-native-size-matters";
+import Metrics from './metrics'
+const {horizontalScale, verticalScale, moderateScale} = Metrics
+import {isJamAtom, isTalkAtom, roomInNumberOfPeopleAtom} from "@/app/atom";
+import {useAtomValue} from "jotai";
+import {useAtom} from "jotai/index";
+// import Animated from "react-native-reanimated";
+
+
+export default function Sec4_Room()  {
+    const roomInNumberOfPeople = useAtomValue(roomInNumberOfPeopleAtom)
+    const [isJam, setIsJam] = useAtom(isJamAtom)
+    const [isTalk, setIsTalk] = useAtom(isTalkAtom)
+    const [count, setCount]= useState(10)
+    const [isExit, setIsExit]= useState(false)
+    const { width, height } = Dimensions.get('window');
+
+    // 初期の位置を右上に設定
+    const [positions, setPositions] = useState(
+        new Array(2).fill(0).map(() => ({
+            top: new Animated.Value(verticalScale(28)),
+            left: new Animated.Value(horizontalScale(100)),
+        }))
+    );
+
+    // アニメーションを順番に実行するための関数
+    const moveImagesSequentially = () => {
+        // 各画像を順番に動かすアニメーション
+        const animations = positions.map((position, index) => {
+            return Animated.sequence([
+                Animated.timing(position.left, {
+                    toValue: horizontalScale(11), // 左端に移動
+                    duration: 2000,
+                    useNativeDriver: false,
+                }),
+                Animated.timing(position.top, {
+                    toValue: verticalScale(51), // 下端に移動
+                    duration: 2000,
+                    useNativeDriver: false,
+                })
+            ]);
+        });
+
+        // すべてのアニメーションを順番に実行
+        Animated.stagger(1000, animations).start(); // 1秒ごとに次の画像が動く
+    };
+
+    useEffect(() => {
+        moveImagesSequentially(); // アニメーション開始
+    }, []);
+
+    // const fadeAnim = new Animated.Value(0)
+    // useEffect(() => {
+    //     Animated.timing(fadeAnim,{
+    //         toValue: 1,
+    //         duration: 1000,
+    //         useNativeDriver: true,
+    //     }).start()
+    // }, []);
+
+    const startExit = ()=>{
+        if(!isExit){
+            setIsExit(true)
+            let countNum=11
+            const countDownTimer = setInterval(()=>{
+                if(countNum<=1){
+                    setIsExit(false)
+                    setCount(10)
+                    setIsJam(false)
+                    setIsTalk(false)
+                    clearInterval(countDownTimer)
+                    console.log("timer_end__")
+                }else{
+                    countNum--
+                    setCount(countNum)
+                }
+            },1000)
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image style={{width: '100%', height:'100%'}}
+                   resizeMode='contain'
+                   source={require('../../assets/images/sec4_room.png')}/>
+            <Pressable style={thisStyles.button} onPress={startExit} >
+                <Text style={thisStyles.buttonText}>退出</Text>
+            </Pressable>
+            <View style={thisStyles.peopleArea}>
+                <Text style={thisStyles.peopleText1}>現在</Text>
+                <Text style={thisStyles.peopleText2}>{roomInNumberOfPeople}</Text>
+                <Text style={thisStyles.peopleText3}>名</Text>
+            </View>
+            <View style={[thisStyles.countArea,{opacity: isExit ? 1:0 }]}>
+                <Text style={thisStyles.countText1}>まもなくルームから退出します</Text>
+                <Text style={thisStyles.countText2}>{count}</Text>
+            </View>
+
+            {positions.map((position, index) => (
+                <Animated.View
+                    key={index}
+                    style={{
+                        position: 'absolute',
+                        top: position.top,
+                        left: position.left,
+                        width: 100,
+                        height: 100,
+                    }}
+                >
+                    <Image
+                        source={require('../../assets/images/cars/car1_0.png')}
+                        // image=${index + 1}` }} // 画像のソース（適宜変更）
+                        style={{ width: 106 }}
+                        resizeMode='contain'
+                    />
+                </Animated.View>
+            ))}
+
+            {/*<Animated.View style={{*/}
+            {/*    opacity: fadeAnim,*/}
+            {/*    position: 'absolute',*/}
+            {/*    top:100}}>*/}
+            {/*    <Image style={{width:106}}*/}
+            {/*           resizeMode='contain'*/}
+            {/*           source={require('../../assets/images/cars/car1_0.png')}/>*/}
+            {/*</Animated.View>*/}
+
+            {/*<Animated.View*/}
+            {/*    style={{*/}
+            {/*        position: 'absolute',*/}
+            {/*        top: position.top, // topの位置をアニメーションで変更*/}
+            {/*        left: position.left, // leftの位置をアニメーションで変更*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    <Image style={{width:106}}*/}
+            {/*           resizeMode='contain'*/}
+            {/*           source={require('../../assets/images/cars/car1_0.png')}/>*/}
+            {/*</Animated.View>*/}
+
+
+        </View>
+    );
+};
+
+const thisStyles = StyleSheet.create({
+    button: {
+        position: 'absolute',
+        top: verticalScale(75),//あとで再計算
+        width: horizontalScale(72),
+        height: verticalScale(9),
+        borderRadius: 40,
+        backgroundColor:'#737373',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonText:{
+        fontWeight: 'bold',
+        fontSize: moderateScale(20),
+        fontFamily: 'BIZ UDPGothic',
+        color:'white',
+
+    },
+    peopleArea: {
+        position: 'absolute',
+        flexDirection: 'row',
+        top: verticalScale(15),//あとで再計算
+        width: horizontalScale(50),
+        height: verticalScale(11),
+    },
+    peopleText1: {
+        fontWeight: 'bold',
+        fontSize: moderateScale(24),
+        fontFamily: 'BIZ UDPGothic',
+        color:'#2B2B2B',
+    },
+    peopleText2: {
+        position: 'absolute',
+        textAlign: 'center',
+        width: '100%',
+        fontWeight: 'bold',
+        fontSize: moderateScale(90),
+        fontFamily: 'BIZ UDPGothic',
+        color:'#1A1311',
+    },
+    peopleText3: {
+        position: 'absolute',
+        right: 0,
+        bottom:0,
+        fontWeight: 'bold',
+        fontSize: moderateScale(24),
+        fontFamily: 'BIZ UDPGothic',
+        color:'#2B2B2B',
+
+    },
+    countArea: {
+        position: 'absolute',
+        alignItems:'center',
+        top: verticalScale(30),//あとで再計算
+        width: horizontalScale(95),
+        height: verticalScale(26),
+        borderRadius: 10,
+        backgroundColor: 'white'
+    },
+    countText1: {
+        position: 'absolute',
+        top: verticalScale(5),//17
+        fontWeight: 'bold',
+        fontSize: moderateScale(22),
+        fontFamily: 'BIZ UDPGothic',
+        color:'black',
+    },
+    countText2: {
+        position: 'absolute',
+        top: verticalScale(12),//46
+        textAlign: 'center',
+        width: '100%',
+        fontWeight: 'bold',
+        fontSize: moderateScale(70),
+        fontFamily: 'BIZ UDPGothic',
+        color:'black',
+    },
+
+})
