b308eee8f8a1bc2cee7acff2ae9cb24848abc5ad
Tsugu <masatsugu_yamada@mail.toyota.co.jp>
2025-01-14 00:36:06 +0900

    待機画面修正（参加しない処理追加、認識不一致処理追加）

--- frontend/app/components/sec1_wait.tsx
+++ frontend/app/components/sec1_wait.tsx
@@ -11,6 +11,9 @@ import {useAtomValue} from "jotai/index";
 import { Audio, Video } from "expo-av";
 import {number} from "prop-types";
 
+let counter = 0
+const repeatNum = 2
+
 // import {Button, View, StyleSheet, Text} from "react-native";
 // import { Audio } from "expo-av";
 // import React, { useState } from "react";
@@ -20,7 +23,7 @@ import {number} from "prop-types";
 type Ami = {
     recording: null | Audio.Recording;
     startRecording: () => void;
-    stopRecording: (apiAddress: string) => Promise<{ isRes: boolean, text: any } | undefined>;
+    stopRecording: (apiAddress: string) => Promise<{ isResYes: boolean, isResNo: boolean, text: any ,isMatch:boolean} | undefined>;
 };
 
 const amiVoice: Ami = {
@@ -114,11 +117,18 @@ const amiVoice: Ami = {
             this.recording = null
 
             //「ひらがな、漢字、全角英字」で判定　（カタカナ、半角は除く）
-            const isRes = !!(resA.includes("はい") ||
+            const isResYes = !!(resA.includes("はい") ||
                 resA.includes("ＯＫ") ||
-                resA.includes("いえす"));
+                resA.includes("いえす"))
 
-            return {isRes: isRes, text: resA }
+            const isResNo = !!(resA.includes("いいえ"))
+
+            const isMatch = !!(resA.includes("はい") ||
+                resA.includes("ＯＫ") ||
+                resA.includes("いえす") ||
+                resA.includes("いいえ"));
+
+            return {isResYes: isResYes, isResNo: isResNo, text: resA, isMatch:isMatch}
         }
     }
 }
@@ -127,9 +137,13 @@ export default function Sec1_Wait()  {
     const [isJam, setIsJam] = useAtom(isJamAtom)//テスト用に設置。本来は位置取得時にSet。
     const [isTalk, setIsTalk] = useAtom(isTalkAtom)
     const apiAddress = useAtomValue(apiAddressAtom)//APIアドレス（頭部分）
-    const [isReording, setIsRecording] = useState(false)
+    const [isRecording, setIsRecording] = useState(false)
     const [recordingText, setRecordingText] = useState('')
     const [isRecordingSuccessful, setIsRecordingSuccessful] = useState(false)
+    const [isSoundFinish, setIsSoundFinish] = useState(false)
+    const [isMatchWord, setIsMatchWord] = useState(false)
+    const [isNotTalk, setIsNotTalk] = useState(false)
+    const [isRetry, setIsRetry] = useState(false)
 
     //車ムービー_ここから_______________________________________
     // const position,setPosition] = useState([
@@ -140,7 +154,7 @@ export default function Sec1_Wait()  {
     const [position2, setPosition2]=useState(
         {top: new Animated.Value(verticalScale(37)),
         left: new Animated.Value(horizontalScale(100))}
-);
+    );
 
     const setEndPoint = (left:Animated.Value, top:Animated.Value,topNum:number,leftNum:number)=>{
         // console.log("carNum;",carNum)
@@ -161,28 +175,63 @@ export default function Sec1_Wait()  {
     }
 
     useEffect(() => {
-
             Animated.loop(
-
                 Animated.sequence([
                     Animated.parallel([
                         Animated.parallel(setEndPoint(position1.left, position1.top, 67, -50)),
                         Animated.parallel(setEndPoint(position2.left, position2.top, 37, 100))
                     ]),
                     Animated.parallel(setEndPoint(position2.left, position2.top, 78, -50)),
-
             ])
             ).start()
     }, []);
     //車ムービー_ここまで_______________________________________
 
+    const fadeAnim1 = new Animated.Value(0);
+    const fadeAnim2 = new Animated.Value(0);
+    const fadeAnim3 = new Animated.Value(0);
+    useEffect(() => {
+        console.log(`[anime]soundFin:${isSoundFinish},recordingSuc:${isRecordingSuccessful},recoding:${isRecording}`)
+        if(isSoundFinish && !isRecordingSuccessful){
+            Animated.timing(fadeAnim1, {
+                toValue: 1,
+                duration: 800,
+                useNativeDriver: true,
+            }).start();
+        }
+        setTimeout(()=>{
+            if(isSoundFinish && !isRecordingSuccessful){
+                Animated.timing(fadeAnim2, {
+                    toValue: 1,
+                    duration: 800,
+                    useNativeDriver: true,
+                }).start();
+            }
+        },800)
+        setTimeout(()=>{
+            if(isSoundFinish && !isRecordingSuccessful){
+                Animated.timing(fadeAnim3, {
+                    toValue: 1,
+                    duration: 800,
+                    useNativeDriver: true,
+                }).start();
+            }
+        },1600)
+    }, [isSoundFinish, isRecordingSuccessful, isRecording]);
+
     const playSound = async () => {
         console.log('load　sound')
+        let soundFile
         if (isJam) {
+            if(isRetry){
+                soundFile = require('../../assets/voice/sec1_wait_retry.mp3')
+            }else if(isNotTalk){
+                soundFile = require('../../assets/voice/sec1_wait_wait.mp3')
+            }else{
+                soundFile = require('../../assets/voice/sec1_read_kakunin.mp3')
+            }
+            const {sound} = await Audio.Sound.createAsync(soundFile);
 
-            const {sound} = await Audio.Sound.createAsync(
-                require('../../assets/voice/sec1_read_kakunin.mp3')
-            );
             // setSound(sound);
             await sound.replayAsync();
             sound.setOnPlaybackStatusUpdate((status) => {
@@ -195,6 +244,7 @@ export default function Sec1_Wait()  {
                 // 2. ステータスが成功かつ loaded であるとわかった時点で
                 //    status は AVPlaybackStatusSuccess として扱える
                 if (status.didJustFinish) {
+                    setIsSoundFinish(true);
                     console.log('音声再生が終了');
                 }
             })
@@ -205,24 +255,29 @@ export default function Sec1_Wait()  {
         setIsRecording(true)
         setRecordingText('')
         setIsRecordingSuccessful(false)
+        setIsRetry(false)
         await amiVoice.startRecording()
         setTimeout(async() => {
-            setIsRecording(false)
             const amiRes = await amiVoice.stopRecording(apiAddress)
+            setIsRecording(false)
 
             if (!amiRes) {
                 return; // amiResの型ガード (TypeScriptエラー回避)
             }
 
             console.log("sec1_amires;",amiRes)
-            setIsTalk(amiRes.isRes)
+            amiRes.isResYes && amiRes.isMatch? setIsTalk(true) : setIsTalk(false)
+            amiRes.isResNo && amiRes.isMatch? setIsNotTalk(true) : setIsNotTalk(false)
             setRecordingText(amiRes.text)
+            !amiRes.isMatch ? setIsRetry(true) : setIsRetry(false)
             setIsRecordingSuccessful(true)
-        }, 2000);
+        }, 3000);
     }
 
-
     useEffect(() => {
+        counter = 0
+        setIsSoundFinish(false);
+        setIsMatchWord(false);
         console.log("isJam",isJam)
         if(isJam){
             console.log("play")
@@ -230,15 +285,47 @@ export default function Sec1_Wait()  {
                 console.log('isJam実行だよ')
                 await playSound()
                 console.log('--playおわり')
-
-                setTimeout(async() => {
-                    await getAmiVoice()
-                }, 2000);
             }
             func().then(()=>{})
         }
         return console.log('')
     }, [isJam]);
+
+    useEffect(() => {
+        if(isSoundFinish){
+            const funcAmi = async () => {
+                await getAmiVoice()
+            }
+            funcAmi().then(()=>{})
+        }
+    }, [isSoundFinish]);
+
+    useEffect(() => {
+        if(isNotTalk){
+            console.log("useE isNotTalk in")
+            playSound()
+            setTimeout(() => {
+                setIsJam(false)
+                setIsNotTalk(false)
+            },3000)
+        }
+    }, [isNotTalk])
+
+    useEffect(() => {
+        if(isRetry){
+            console.log("useE isRetry in")
+            setIsNotTalk(false)
+            const funcRetry =  async() => {
+                setIsSoundFinish(false)
+                await playSound()
+            }
+            const funcNotTalk = () => {
+                setIsNotTalk(true)
+                setIsRetry(false)
+            }
+            repeatNum >= ++counter ? funcRetry() : funcNotTalk()
+        }
+    }, [isRetry]);
 
     //             sound.unloadAsync();
 
@@ -272,27 +359,61 @@ export default function Sec1_Wait()  {
 
 
             <View style={{position:'absolute', top:'10%', backgroundColor:'yellow'}}>
-            <Button title='渋滞発生（クリック）' onPress={()=>{setIsJam(!isJam); setIsRecordingSuccessful(false)}}
-                    color="red" accessibilityLabel="button"/>
+                <Button title='渋滞発生（クリック）' onPress={()=>{setIsJam(!isJam); setIsRecordingSuccessful(false)}}
+                        color="red" accessibilityLabel="button"/>
             </View>
 
-                <View style={[thisStyles.jamArea, { opacity: isJam ? 1:0 }]}>
-                    <View style={thisStyles.jamArea2}>
-                    <Text style={thisStyles.jamText1}>
+            <View style={[thisStyles.jamArea, { opacity: isJam ? 0.9:0 }]}>
+                <View style={thisStyles.jamArea2}>
+                    <Text style={[thisStyles.jamText1,{top:'0%'}]}>
                         渋滞に入りました。{"\n"}
                         周囲の人と会話ができます。{"\n"}
                     </Text>
                     <Text style={thisStyles.jamText2}>会話に参加しますか？</Text>
                 </View>
-                <Text style={[thisStyles.overlayText,
-                    {top:'60%', fontSize:moderateScale(30), color:'red',  opacity: isReording ? 1:0 , backgroundColor:"pink"}]}>音声認識中</Text>
-                <Text style={[thisStyles.overlayText,
-                    {top:'60%', fontSize:moderateScale(20), color:'red',  opacity: !isReording && isRecordingSuccessful ? 1:0 , backgroundColor:"pink"}]}>
-                        通話しません。{"\n"}
-                    認識した音声：{"\n"}
-                    {recordingText}
-                </Text>
+                <View style={thisStyles.jamArea3}>
+                    <Text style={[thisStyles.jamText3,{top:'50%',opacity: isRetry ? 1:0}]}>
+                        もう一度話してください
+                    </Text>
                 </View>
+                <Text style={[thisStyles.overlayText,
+                    {top:'80%', fontSize:moderateScale(24), color:'#00B4AA',  opacity: isNotTalk ? 1:0}]}>
+                    待機画面に戻ります
+                </Text>
+            </View>
+
+            <View style={[thisStyles.micArea,{opacity: isSoundFinish  && !isRecordingSuccessful ? 1:0}]}>
+                <Image style={{ width:horizontalScale(27)}}
+                       resizeMode='contain'
+                       source={require('../../assets/images/mic.png')}/>
+            </View>
+            <View style={[thisStyles.micArea, { opacity: isSoundFinish && !isRecordingSuccessful ? 1:0}]}>
+                <Animated.View style={{ opacity: fadeAnim1 }}>
+                    <Image
+                      style={{ width: horizontalScale(33) }}
+                      resizeMode='contain'
+                      source={require('../../assets/images/circle1.png')}
+                    />
+                </Animated.View>
+            </View>
+            <View style={[thisStyles.micArea, { opacity: isSoundFinish && !isRecordingSuccessful ? 1:0}]}>
+                <Animated.View style={{ opacity: fadeAnim2 }}>
+                    <Image
+                      style={{ width: horizontalScale(46) }}
+                      resizeMode='contain'
+                      source={require('../../assets/images/circle2.png')}
+                    />
+                </Animated.View>
+            </View>
+            <View style={[thisStyles.micArea, { opacity: isSoundFinish && !isRecordingSuccessful ? 1:0}]}>
+                <Animated.View style={{ opacity: fadeAnim3 }}>
+                    <Image
+                      style={{ width: horizontalScale(59) }}
+                      resizeMode='contain'
+                      source={require('../../assets/images/circle3.png')}
+                    />
+                </Animated.View>
+            </View>
         </View>
     );
 };
@@ -326,7 +447,7 @@ const thisStyles = StyleSheet.create({
         justifyContent: "center",
         top: verticalScale(36),
         width: horizontalScale(95),
-        height: verticalScale(17),
+        height: verticalScale(28),
         fontWeight: 'bold',
         textAlign: 'center',
         backgroundColor: 'white',
@@ -334,6 +455,9 @@ const thisStyles = StyleSheet.create({
 
     },
     jamArea2:{
+        justifyContent: 'center',
+    },
+    jamArea3:{
         justifyContent: 'center',
     },
     jamText1: {
@@ -347,5 +471,31 @@ const thisStyles = StyleSheet.create({
         fontFamily: 'BIZ UDPGothic',
         textAlign: 'center',
     },
+    jamText3: {
+        fontSize: moderateScale(20),
+        fontWeight: 'bold',
+        fontFamily: 'BIZ UDPGothic',
+        textAlign: 'center',
+        color: '#D70915'
+    },
+    jamText4: {
+        fontSize: moderateScale(24),
+        fontWeight: 'bold',
+        fontFamily: 'BIZ UDPGothic',
+        textAlign: 'center',
+        color: '#00B4AA'
+    },
+
+micArea:{
+    position: 'absolute',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+    justifyContent: "center",
+    top: verticalScale(70),
+    width: horizontalScale(95),
+    height: verticalScale(17),
+
+}
+
 
 });
