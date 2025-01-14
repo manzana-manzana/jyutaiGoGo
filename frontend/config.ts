534feba26602d7f8a9c8331718a99192a657e6f8
Tora <toranosuke_kakizono@mail.toyota.co.jp>
2025-01-14 10:57:45 +0900

    config本番ように変更

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,2 +1,8 @@
-export const BASE_URL = "http://192.168.11.5:8080"; // 開発用
-// export const BASE_URL = "https://jyutaigogo-4a13b4c42afd.herokuapp.com"; // 本番用
+// 在宅用
+// export const BASE_URL = "http://192.168.11.5:8080"; // 開発用
+
+// 本番環境Heroku用
+export const BASE_URL = "https://jyutaigogo-4a13b4c42afd.herokuapp.com"; // 本番用
+
+// テザリング用
+// export const BASE_URL = "http://172.20.10.4:8080";
